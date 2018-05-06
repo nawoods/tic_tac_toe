13976a1c04c4935bcf4b51037a15ada643484f60
Nick Woods <nicholas.andrew.woods@gmail.com>
2018-05-06 05:25:12 +0000

    Begin checking for game end conditions

--- game.rb
+++ game.rb
@@ -24,4 +24,25 @@ class Game
       "    #{@spaces[6]} | #{@spaces[7]} | #{@spaces[8]}\n"\
       "      |   |   \n"
   end
+  
+  def move(player, space_number)
+    space = @spaces[space_number]
+    space.player = player
+  end
+  
+  private
+  
+  def check_if_game_over(player)
+    winning_combos = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
+                      [0, 3, 6], [1, 4, 7], [2, 5, 8],
+                      [0, 4, 8], [2, 4, 6]]
+    winning_combos each do |combo|
+      return unless combo.all? { |space| space.player == player }
+      # insert code for player winning game here
+    end
+    
+    if @spaces.all? { |space| !space.player.nil? }
+      # insert code for draw game here
+    end
+  end
 end
